a92b3904f64c8731b11630543aa6ea4caa9af0db
Naga Sai Srinivas Maridu <128402948+nagasais@users.noreply.github.com>
2023-09-12 15:06:36 +0530

    Add files via upload

---
+++ buffer.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+class buffer{
+	public static void main(String[] args) throws Exception{
+		InputStreamReader ir=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(ir);
+		System.out.println("Enter name,age,gender");
+		String name=br.readLine();
+		int age=Integer.parseInt(br.readLine());
+		String gender=br.readLine();
+		System.out.println("Name : "+name+"\n age : "+age+"\n Gender : "+gender);
+	}
+}
